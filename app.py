84800ba683d21b99e461e17076518e955ff15fc9
seblful <alesha.chimba@gmail.com>
2024-03-20 15:41:53 +0300

    [EDIT] add ocr

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from modules.instructors import Instructs
+from modules.instructors import Instruct, InstructsOCR
 
 import os
 
@@ -9,10 +9,14 @@ INSTR_DIR = os.path.join(HOME, 'instructions')
 def main():
     pdf_path = os.path.join(INSTR_DIR, '21_07_3165_i.pdf')
     pdf_url = 'https://www.rceth.by/NDfiles/instr/21_07_3165_i.pdf'
-    instr = Instructs(instr_dir=INSTR_DIR,
-                      pdf_url=pdf_url, pdf_path=pdf_path)
+    instruct = Instruct(instr_dir=INSTR_DIR,
+                        pdf_url=pdf_url)  # , pdf_path=pdf_path)
+    print(instruct.pdf_path)
 
-    print(instr.instr_imgs)
+    image = instruct.instr_imgs[0]
+
+    ocr = InstructsOCR()
+    ocr.predict(instruct=instruct)
 
 
 if __name__ == '__main__':
