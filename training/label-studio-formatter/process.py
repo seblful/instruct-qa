641f72fafe4bc7e94b7a070f2475e2eeedbf769c
seblful <alesha.chimba@gmail.com>
2024-04-02 22:52:48 +0300

    [EDIT] minor changes

--- training/label-studio-formatter/process.py
+++ training/label-studio-formatter/process.py
@@ -3,13 +3,14 @@ from labels import LSLabelFormatter
 import os
 
 json_input_path = "or_bb_labels.json"
-json_output_path = "polygon_labels.json"
+json_output_path = "polygon_labels_conv.json"
 
 
 def main():
     label_formatter = LSLabelFormatter()
 
-    label_formatter.convert_or_bbox_to_polygon(json_input_path=json_input_path)
+    label_formatter.convert_or_bbox_to_polygon(json_input_path=json_input_path,
+                                               json_output_path=json_output_path)
 
 
 if __name__ == "__main__":
