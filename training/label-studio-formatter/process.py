776b6a100c7b7d72af0d92e531b89b4a8a4f69a5
seblful <alesha.chimba@gmail.com>
2024-04-05 11:50:50 +0300

    [EDIT] add visualization

--- training/label-studio-formatter/process.py
+++ training/label-studio-formatter/process.py
@@ -3,14 +3,15 @@ from labels import LSLabelFormatter
 import os
 
 json_input_path = "inputs/or_bb_labels.json"
-json_output_path = "outputs/polygon_labels_conv.json"
+json_output_path = "outputs/polygon_labels_converted.json"
 
 
 def main():
     label_formatter = LSLabelFormatter()
 
     label_formatter.convert_or_bbox_to_polygon(json_input_path=json_input_path,
-                                               json_output_path=json_output_path)
+                                               json_output_path=json_output_path,
+                                               visualize=False)
 
 
 if __name__ == "__main__":
