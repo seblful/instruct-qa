1ff59713984fb4bb6ee84fc1c1c604773a1d6ea5
seblful <alesha.chimba@gmail.com>
2024-04-07 18:38:16 +0300

    [EDIT] convert labels with universal function

--- training/label-studio-formatter/process.py
+++ training/label-studio-formatter/process.py
@@ -9,9 +9,11 @@ json_output_path = "outputs/polygon_labels_converted.json"
 def main():
     label_formatter = LSLabelFormatter()
 
-    label_formatter.convert_or_bbox_to_polygon(json_input_path=json_input_path,
-                                               json_output_path=json_output_path,
-                                               visualize=False)
+    label_formatter.convert_labels(label_from="rectangle",
+                                   label_to="polygon",
+                                   json_input_path=json_input_path,
+                                   json_output_path=json_output_path,
+                                   visualize=True)
 
 
 if __name__ == "__main__":
