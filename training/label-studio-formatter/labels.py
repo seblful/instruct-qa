7e623cf47f634a73c7c71942f74df9bd3bd02c18
seblful <alesha.chimba@gmail.com>
2024-04-09 20:59:41 +0300

    [EDIT] add new polygon to url func

--- training/label-studio-formatter/labels.py
+++ training/label-studio-formatter/labels.py
@@ -151,18 +151,150 @@ class PolygonLabel:
 
         return x_max, y_max
 
-    def mask_to_rle(self, mask):
-        flattened_mask = mask.ravel(order="F")
-        diff_arr = np.diff(flattened_mask)
-        nonzero_indices = np.where(diff_arr != 0)[0] + 1
-        lengths = np.diff(np.concatenate(
-            ([0], nonzero_indices, [len(flattened_mask)])))
+    def base_rle_encode(self, inarray):
+        """run length encoding. Partial credit to R rle function.
+        Multi datatype arrays catered for including non Numpy
+        returns: tuple (runlengths, startpositions, values)"""
+        ia = np.asarray(inarray)  # force numpy
+        n = len(ia)
+        if n == 0:
+            return None, None, None
+        else:
+            y = ia[1:] != ia[:-1]  # pairwise unequal (string safe)
+            i = np.append(np.where(y), n - 1)  # must include last element posi
+            z = np.diff(np.append(-1, i))  # run lengths
+            p = np.cumsum(np.append(0, z))[:-1]  # positions
+            return z, p, ia[i]
 
-        # note that the odd counts are always the numbers of zeros
-        if flattened_mask[0] == 1:
-            lengths = np.concatenate(([0], lengths))
+    def bits2byte(self,
+                  arr_str,
+                  n=8):
+        """Convert bits back to byte
 
-        rle = lengths.tolist()
+        :param arr_str:  string with the bit array
+        :type arr_str: str
+        :param n: number of bits to separate the arr string into
+        :type n: int
+        :return rle:
+        :type rle: list
+        """
+        rle = []
+        numbers = [arr_str[i: i + n] for i in range(0, len(arr_str), n)]
+        for i in numbers:
+            rle.append(int(i, 2))
+        return rle
+
+    def encode_rle(self,
+                   arr,
+                   wordsize=8,
+                   rle_sizes=[3, 4, 8, 16]):
+        """Encode a 1d array to rle
+
+        :param arr: flattened np.array from a 4d image (R, G, B, alpha)
+        :type arr: np.array
+        :param wordsize: wordsize bits for decoding, default is 8
+        :type wordsize: int
+        :param rle_sizes:  list of ints which state how long a series is of the same number
+        :type rle_sizes: list
+        :return rle: run length encoded array
+        :type rle: list
+        """
+        # Set length of array in 32 bits
+        num = len(arr)
+        numbits = f'{num:032b}'
+
+        # put in the wordsize in bits
+        wordsizebits = f'{wordsize - 1:05b}'
+
+        # put rle sizes in the bits
+        rle_bits = ''.join([f'{x - 1:04b}' for x in rle_sizes])
+
+        # combine it into base string
+        base_str = numbits + wordsizebits + rle_bits
+
+        # start with creating the rle bite string
+        out_str = ''
+        for length_reeks, p, value in zip(*self.base_rle_encode(arr)):
+            # TODO: A nice to have but --> this can be optimized but works
+            if length_reeks == 1:
+                # we state with the first 0 that it has a length of 1
+                out_str += '0'
+                # We state now the index on the rle sizes
+                out_str += '00'
+
+                # the rle size value is 0 for an individual number
+                out_str += '000'
+
+                # put the value in a 8 bit string
+                out_str += f'{value:08b}'
+                state = 'single_val'
+
+            elif length_reeks > 1:
+                state = 'series'
+                # rle size = 3
+                if length_reeks <= 8:
+                    # Starting with a 1 indicates that we have started a series
+                    out_str += '1'
+
+                    # index in rle size arr
+                    out_str += '00'
+
+                    # length of array to bits
+                    out_str += f'{length_reeks - 1:03b}'
+
+                    out_str += f'{value:08b}'
+
+                # rle size = 4
+                elif 8 < length_reeks <= 16:
+                    # Starting with a 1 indicates that we have started a series
+                    out_str += '1'
+                    out_str += '01'
+
+                    # length of array to bits
+                    out_str += f'{length_reeks - 1:04b}'
+
+                    out_str += f'{value:08b}'
+
+                # rle size = 8
+                elif 16 < length_reeks <= 256:
+                    # Starting with a 1 indicates that we have started a series
+                    out_str += '1'
+
+                    out_str += '10'
+
+                    # length of array to bits
+                    out_str += f'{length_reeks - 1:08b}'
+
+                    out_str += f'{value:08b}'
+
+                # rle size = 16 or longer
+                else:
+                    length_temp = length_reeks
+                    while length_temp > 2**16:
+                        # Starting with a 1 indicates that we have started a series
+                        out_str += '1'
+
+                        out_str += '11'
+                        out_str += f'{2 ** 16 - 1:016b}'
+
+                        out_str += f'{value:08b}'
+                        length_temp -= 2**16
+
+                    # Starting with a 1 indicates that we have started a series
+                    out_str += '1'
+
+                    out_str += '11'
+                    # length of array to bits
+                    out_str += f'{length_temp - 1:016b}'
+
+                    out_str += f'{value:08b}'
+
+        # make sure that we have an 8 fold lenght otherwise add 0's at the end
+        nzfill = 8 - len(base_str + out_str) % 8
+        total_str = base_str + out_str
+        total_str = total_str + nzfill * '0'
+
+        rle = self.bits2byte(total_str)
 
         return rle
 
@@ -178,20 +310,20 @@ class PolygonLabel:
         # size = (int(x_max * image_width / 100),
         #         int(y_max * image_height / 100))
 
-        # Generate mask from polygons
+        # Generate mask from polygons and preprocess it
         mask = np.zeros((image_height, image_width), dtype=np.int32)
-        mask = cv2.fillPoly(mask, [relative_points], 1)
-
-        image = Image.fromarray(mask * 255)
-        image.show()
+        mask = cv2.fillPoly(mask, [relative_points], 255)
+        mask = mask.astype(np.uint8)
+        array = np.repeat(mask.ravel(), 4)
 
         # Convert to rle
-        rle = self.mask_to_rle(mask)
+        rle = self.encode_rle(array)
 
         return rle
 
 
 class BrushLabel:
+
     pass
 
 
