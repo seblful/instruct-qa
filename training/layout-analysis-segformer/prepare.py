9afe8f7a4af013c7beab12bce81e860af9e2d643
seblful <alesha.chimba@gmail.com>
2024-04-14 22:56:07 +0300

    [EDIT] create dataset

--- training/layout-analysis-segformer/prepare.py
+++ training/layout-analysis-segformer/prepare.py
@@ -16,10 +16,14 @@ images_dir = os.path.join(training_dir, 'ls-input-data')
 
 
 def main():
+    # Instantiate DatasetCreator object
     dataset_creator = DatasetCreator(raw_data_dir=raw_data_dir,
                                      images_dir=images_dir,
                                      dataset_dir=dataset_dir,
                                      train_split=0.8)
+
+    # Creating masks from polygons json and split data into train, val and test datasets
+    dataset_creator.process()
 
 
 if __name__ == "__main__":
