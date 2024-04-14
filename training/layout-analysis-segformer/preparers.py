70bbf534cb22d3cd64cf1fb7414278d74c3e005d
seblful <alesha.chimba@gmail.com>
2024-04-14 16:54:36 +0300

    [EDIT] convert and save mask

--- training/layout-analysis-segformer/preparers.py
+++ training/layout-analysis-segformer/preparers.py
@@ -1,5 +1,29 @@
 import os
+import json
 import random
+
+import numpy as np
+from PIL import Image
+import cv2
+
+
+class PolygonLabel():
+    def __init__(self,
+                 points,
+                 label):
+        self.points = points
+        self.label = label
+
+    def __repr__(self):
+        return f"PolygonLabel: points={self.points}, label='{self.label}'"
+
+    def convert_to_relative(self,
+                            image_width,
+                            image_height):
+        points = [(x * image_width / 100, y * image_height / 100)
+                  for x, y in self.points]
+
+        return points
 
 
 class DatasetCreator():
@@ -11,6 +35,7 @@ class DatasetCreator():
         # Data dirs
         self.inputs_dir = raw_data_dir
         self.images_dir = images_dir
+        self.labels_dir = os.path.join(raw_data_dir, 'labels')
         self.dataset_dir = dataset_dir
 
         # Input dirs
@@ -115,3 +140,93 @@ class DatasetCreator():
             id2label = {k: v for k, v in enumerate(classes)}
 
         return id2label
+
+    def _read_json(self, json_input_path):
+
+        with open(json_input_path) as json_file_input:
+            json_dict = json.load(json_file_input)
+
+        return json_dict
+
+    def __get_polygons(self, task):
+        # Retrieve annotations and results
+        annotation = task['annotations'][0]
+        result = annotation['result']
+
+        # Create list to store polygons and labels
+        polygons = []
+
+        # Process if result is not blank
+        if result:
+            # Get image width and image height
+            image_width, image_height = result[0]['original_width'], result[0]['original_height']
+            # Iterating through results
+            for res in result:
+                # Get value from result
+                value = res['value']
+
+                # Get polygon and label
+                polygon = PolygonLabel(points=value['points'],
+                                       label=value['polygonlabels'][0])
+
+                # Append labels to lists
+                polygons.append(polygon)
+
+        return polygons, image_width, image_height
+
+    def __convert_polygons_to_mask(self,
+                                   polygons,
+                                   image_width,
+                                   image_height):
+        # Generate mask
+        mask = np.zeros((image_height, image_width), dtype=np.int32)
+
+        # Iterating through polygons
+        for polygon in polygons:
+            # Converts points to relative
+            relative_points = polygon.convert_to_relative(
+                image_width, image_height)
+            relative_points = np.array(relative_points, dtype=np.int32)
+
+            # Fill mask
+            mask = cv2.fillPoly(
+                mask, [relative_points], self.label2id[polygon.label])
+
+        # Convert mask to uint8
+        mask = mask.astype(np.uint8)
+
+        return mask
+
+    def __save_mask(self,
+                    mask_array,
+                    image_name):
+        # Create mask path
+        mask_name = os.path.splitext(image_name)[0] + '.png'
+        mask_path = os.path.join(self.labels_dir, mask_name)
+
+        # Convert mask array to image and save it
+        mask_image = Image.fromarray(mask_array)
+        mask_image.save(mask_path)
+
+        return None
+
+    def _create_mask_images(self):
+        # Read json_polygon_path
+        json_input = self._read_json(self.json_polygon_path)
+
+        # Iterating through tasks
+        for task in json_input:
+            # Get polygons and labels
+            polygons, image_width, image_height = self.__get_polygons(task)
+
+            # Get image path and mask path
+            image_name = os.path.basename(task["data"]["image"]).split('-')[1]
+
+            # Create mask
+            mask_array = self.__convert_polygons_to_mask(polygons=polygons,
+                                                         image_width=image_width,
+                                                         image_height=image_height)
+
+            # Save mask as images
+            self.__save_mask(mask_array=mask_array,
+                             image_name=image_name)
