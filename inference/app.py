673c8ef002c9f201a58856e325230c312b13fd94
seblful <alesha.chimba@gmail.com>
2024-04-22 21:15:15 +0300

    [EDIT] minor changes

--- inference/app.py
+++ inference/app.py
@@ -1,4 +1,4 @@
-from modules.instructors import Instruct, InstructsOCR
+from modules.instructors import Instruction, InstructsOCR
 
 import os
 
@@ -9,14 +9,11 @@ INSTR_DIR = os.path.join(DATA_DIR, 'instructions')
 def main():
     pdf_path = os.path.join(INSTR_DIR, '21_07_3165_i.pdf')
     pdf_url = 'https://www.rceth.by/NDfiles/instr/21_07_3165_i.pdf'
-    instruct = Instruct(instr_dir=INSTR_DIR,
-                        pdf_url=pdf_url)  # , pdf_path=pdf_path)
-    print(instruct.instr_imgs[2].save('test_image.jpg'))
+    instruction = Instruction(instr_dir=INSTR_DIR,
+                              pdf_url=pdf_url)  # , pdf_path=pdf_path)
 
-    image = instruct.instr_imgs[0]
-
-    ocr = InstructsOCR()
-    ocr.predict(instruct=instruct)
+    image = instruction.instr_imgs[0]
+    image.show()
 
 
 if __name__ == '__main__':
